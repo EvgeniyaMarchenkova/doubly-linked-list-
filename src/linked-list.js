4e798facffd3d38e4c3ff06dc66f3b37a5e786d3
EvgeniyaMarchenkova <evgeniova@gmail.com>
2017-01-29 01:31:33 +0300

    doubly-linked-list/feature 7 methods added

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,82 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;
 
-    append(data) {}
+    }
+    append(data) {
+        var newNode= new Node();
+        newNode.data = data;
 
-    head() {}
+        if (this.length !== 0) {
+            this._tail.next = newNode;
+            newNode.prev=this._tail;
+            this._tail = newNode;
+        }
+        else  {
+            this._tail = newNode;
+            this._head = newNode;
 
-    tail() {}
+        }
 
-    at(index) {}
+        this.length++;
+        return newNode;
+    }
 
-    insertAt(index, data) {}
+    head() {
+        return this._head.data;
 
-    isEmpty() {}
+    }
 
-    clear() {}
+    tail() {
+        return this._tail.data;
+    }
+
+    getNodeByIndex(index) {
+        var currentNode = this._head;
+        for (var i=0; i<index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode;
+    }
+
+    at(index) {
+        return this.getNodeByIndex(index).data;
+    }
+
+    insertAt(index, data) {
+        var currentNode = this.getNodeByIndex(index);
+        currentNode.data=data;
+        return currentNode.data;
+
+    }
+
+    isEmpty() {
+        if (this.length) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    clear() {
+
+    }
 
     deleteAt(index) {}
 
-    reverse() {}
+    reverse() {
+
+
+    }
 
     indexOf(data) {}
 }
 
 module.exports = LinkedList;
+
+
